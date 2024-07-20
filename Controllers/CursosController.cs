182393175592d9c6922078e9fc00e317baf5f8f7
RandallRO01 <r.josue.ruiz.oreamuno@gmail.com>
2024-07-20 01:32:39 -0600

    Implementacion del contenido de los cursos

---
+++ Controllers/CursosController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoCanvas.Services;
+
+namespace ProyectoCanvas.Controllers
+{
+    public class CursosController : Controller
+    {
+        private readonly IRepositorioCursos _repositorioCursos;
+        private readonly ILogger<HomeController> _logger;
+
+        public CursosController(IRepositorioCursos repositorioCursos, ILogger<HomeController> logger)
+        {
+            _repositorioCursos = repositorioCursos;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            var curso = await _repositorioCursos.ObtenerCursoPorId(id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+            ViewBag.CourseId = id;
+            ViewBag.CourseName = curso.NombreCurso;
+            ViewBag.CourseDescription = curso.Descripcion;
+            return View(curso);
+        }
+
+        public async Task<IActionResult> Hogar(int id)
+        {
+            var curso = await _repositorioCursos.ObtenerCursoPorId(id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+            ViewBag.CourseId = id;
+            ViewBag.CourseName = curso.NombreCurso;
+            ViewBag.CourseDescription = curso.Descripcion;
+            ViewBag.CourseCuatri = curso.Cuatrimestre;
+            return View(curso);
+        }
+
+        public IActionResult Asignaciones(int id)
+        {
+            ViewBag.CourseId = id;
+            return View();
+        }
+        public IActionResult Notas(int id)
+        {
+            ViewBag.CourseId = id;
+            return View();
+        }
+        public IActionResult Asistencia(int id)
+        {
+            ViewBag.CourseId = id;
+            return View();
+        }
+        public IActionResult Anuncios(int id)
+        {
+            ViewBag.CourseId = id;
+            return View();
+        }
+        public IActionResult Personas(int id)
+        {
+            ViewBag.CourseId = id;
+            return View();
+        }
+    }
+}
